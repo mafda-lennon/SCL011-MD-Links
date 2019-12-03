cafbb8480025fee0d7964537bf5121e989d844c9
mafda-lennon <mf.lennon@gmail.com>
2019-12-03 18:28:44 -0300

    reading file ok. Test are not functional

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+const fs = require ('fs');
+const path = require('path');
+let file = process.argv[2];
+
+function readFile(file){
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) throw err
+    console.log(data)
+  })
+  
+}
+
+readFile(file);
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,15 @@
 const mdLinks = require('../');
 
+describe('validPath', () => {
+
+  it('should return true if is a file', () => {
+    console.log()
+  });
+
+  it('should return true if extension is ".md"', () => {
+    console.log()
+  });
+});
 
 describe('mdLinks', () => {
 
