7f4eaa55f4c67b9288b20b6ab4c28313936787ff
mafda-lennon <mf.lennon@gmail.com>
2019-12-04 10:22:25 -0300

    readme w/how to install. Promise to know if is File or Directory.

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ const fs = require ('fs');
 const path = require('path');
 let file = process.argv[2];
 
+function validPath(){
+  return new Promise(function(resolve, reject){
+    fs.lstat(file, (err, stats) => {
+      if(err){
+        reject(new Error("Archivo o carpeta inexistente"))
+      }else if(mdFile(file)){
+        return readFile(file);
+      }else if(stats.isDirectory() === true){
+        fs.readdir(file, 'utf8',(err,file)=>{
+          file.forEach(element => {
+            if(mdFile(element)){
+              readFile(file+'/'+element);
+            }            
+          });
+      })
+    }
+    resolve(stats);
+  })
+  })
+}
+
+
+const mdFile = (file =>{
+  if (file.slice(-3) == '.md'){
+    return true;
+  }else {
+    return false;
+  }
+})
+
 function readFile(file){
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err
@@ -11,3 +41,4 @@ function readFile(file){
 }
 
 readFile(file);
+validPath(file);
